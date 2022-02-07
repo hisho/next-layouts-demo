5becfe29bcf1e884a9468e88dd5216a7b0a31d0d
hisho <hisho.web@gmail.com>
2022-02-07 18:01:18 +0900

    ✨ create Header

---
+++ src/component/ui/Header/Header.tsx
@@ -0,0 +1,15 @@
+import {memo} from "react";
+
+let render = 0
+
+export const Header = memo(() => {
+  console.log(`render${render}`);
+  render++
+  return (
+    <header>
+      header
+    </header>
+  )
+})
+
+Header.displayName = 'header'
