45cf0a3e9574e5c18c3a939807a458f831b15ff2
hisho <hisho.web@gmail.com>
2022-02-07 18:02:24 +0900

    ✨ create Layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import {ReactElement} from "react";
+import {Footer, Header} from "@src/component/ui";
+
+type LayoutProps = Required<{
+  readonly children: ReactElement
+}>
+
+export const Layout = ({children}:LayoutProps) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+)
